052f38f66754315da6da63e40ab31a91c6ed9def
Shivang Sharma <shivang.sharma.pro@gmail.com>
2026-02-11 16:43:03 +0530

    Connected PostgreSql

--- app/app.js
+++ app/app.js
@@ -1,9 +1,24 @@
 const http = require('http');
-const fs = require('fs');
+const { Client } = require('pg');
 
-const server = http.createServer((req, res) => {
-  fs.appendFileSync('visits.txt', 'New visit\n');
-  res.end("Visit recorded 🚀");
+const client = new Client({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 5432,
+});
+
+client.connect()
+  .then(() => console.log("Connected to PostgreSQL"))
+  .catch(err => console.error("DB connection error", err));
+
+const server = http.createServer(async (req, res) => {
+  await client.query("CREATE TABLE IF NOT EXISTS visits (id SERIAL PRIMARY KEY)");
+  await client.query("INSERT INTO visits DEFAULT VALUES");
+  const result = await client.query("SELECT COUNT(*) FROM visits");
+
+  res.end(`Total visits: ${result.rows[0].count}`);
 });
 
 server.listen(3000, () => {
