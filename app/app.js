67d3b28245caa268979bc0ef9a479200092bf6b4
Shivang Sharma <shivang.sharma.pro@gmail.com>
2026-02-11 15:10:43 +0530

    Added Visits log

--- app/app.js
+++ app/app.js
@@ -1,10 +1,11 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("Docker + DevOps Learning 🚀");
+  fs.appendFileSync('visits.txt', 'New visit\n');
+  res.end("Visit recorded 🚀");
 });
 
 server.listen(3000, () => {
-  console.log("App running on port 3000");
+  console.log("Server running on port 3000");
 });
